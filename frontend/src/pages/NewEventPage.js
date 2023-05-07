2ab565647d638635a8a2e8cfd217596d3d71727f
ace-brown <kazem_abdi@yahoo.com>
2023-05-07 18:03:15 +0200

    refactor the routes

--- frontend/src/pages/NewEventPage.js
+++ frontend/src/pages/NewEventPage.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const NewEventPage = () => {
-  return (
-    <div>NewEventPage</div>
-  )
-}
+  return <div>NewEventPage</div>;
+};
 
-export default NewEventPage
+export default NewEventPage;

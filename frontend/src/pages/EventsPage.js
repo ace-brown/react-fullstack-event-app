9d75d2de0405c420f1356fe6e58c93f6baa42a2e
ace-brown <kazem_abdi@yahoo.com>
2023-05-07 22:30:23 +0200

    add loader func

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,38 +1,24 @@
-import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
 const EventsPage = () => {
-  const [error, setError] = useState();
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-
-      const response = await fetch("http://localhost:8080/events");
-
-      if (!response.ok) {
-        setError("Fetching events failed!");
-      } else {
-        const jsonData = await response.json();
-        setFetchedEvents(jsonData.events);
-      }
-      setIsLoading(false);
-    };
-    fetchData();
-  }, []);
+  const events = useLoaderData();
 
   return (
     <>
-      <div style={{ textAlign: "center" }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+      <EventsList events={events} />
     </>
   );
 };
 
 export default EventsPage;
+
+export const loader = async () => {
+  const response = await fetch("http://localhost:8080/events");
+  if (!response.ok) {
+  } else {
+    const respData = await response.json();
+    return respData.events;
+  }
+};

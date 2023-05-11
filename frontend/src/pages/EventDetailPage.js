7f35f69ea80da253f3aa7560d8e0767d890af09b
ace-brown <kazem_abdi@yahoo.com>
2023-05-11 17:29:57 +0200

    DetailPage.js refactored with loader func

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 const EventDetailPage = () => {
-  const params = useParams();
+  const data = useLoaderData();
 
-  return (
-    <>
-      <h1>Event Id: {params.eventId}</h1>
-      <div>EventDetailPage</div>
-    </>
-  );
+  return <EventItem event={data.event} />;
 };
 
 export default EventDetailPage;
+
+export const loader = async ({ request, params }) => {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch the information for the selected event." },
+      { status: 500 }
+    );
+  }
+  return response;
+};
